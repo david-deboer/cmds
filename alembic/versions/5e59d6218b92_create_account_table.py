1d0f64d52be0b5a6219765143c2bf345c378d1ea
Dave DeBoer <ddeboer@berkeley.edu>
2022-03-31 11:58:39 -0700

    add alembic versions

---
+++ alembic/versions/5e59d6218b92_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 5e59d6218b92
+Revises: 
+Create Date: 2022-03-31 18:34:28.501838+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5e59d6218b92'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
